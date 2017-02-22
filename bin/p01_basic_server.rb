e695e41ffe8e556c3d4c81601edd4a316f507bc4
Andrea del Rio <adelrio@gmail.com>
2017-02-21 16:57:30 -0800

    render and redirect methods built + "double render error"

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,7 +1,27 @@
 require 'rack'
 
+
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+
+# Rack::Server.start(
+#   app: Proc.new do |env|
+#     [ '200', { 'Content-Type' => 'text/html' }, [ 'hello world' ] ]
+#   end
+#                   )
+
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/plain'
+  res.write("Hello World!")
+  res.finish
+end
+
 Rack::Server.start(
-  app: Proc.new do |env|
-    [ '200', { 'Content-Type' => 'text/html' }, [ 'hello world' ] ]
-  end
-                  )
+  app: app,
+  Port: 3000
+)
--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,25 +8,51 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'double render error' if already_built_response?
+
+    res["Location"] = url
+    res.status = 302
+
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'double render error' if already_built_response?
+
+    res.write(content)
+    res['Content-Type'] = content_type
+
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_fname}"
+    )
+    template_code = File.read(template_name)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
   end
 
   # method exposing a `Session` object
@@ -37,4 +63,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
